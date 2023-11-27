0e1850d428cdb90287bf71e5ff45000d8c5905a6
hafizhBimo <hfzhbimo@gmail.com>
2023-11-27 17:16:45 +0700

    add button to add product

--- pages/products/list_products.tsx
+++ pages/products/list_products.tsx
@@ -1,9 +1,17 @@
 import Table from "@/components/Table/Table";
+import { useRouter } from 'next/router';
 
 export default function ListProducts() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push('/products/form');
+  };
   return (
     <div className="listProductWrapper">
       <h1>Product List</h1>
+      <div className="buttonWrapper">
+      <button onClick={handleClick}>add product</button>
+      </div>
       <Table />
     </div>
   );

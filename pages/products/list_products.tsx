f044998a5c18e3f5591aa96e6f4b0ffb778f7856
hafizhBimo <hfzhbimo@gmail.com>
2023-11-27 16:14:39 +0700

    styling product list page

--- pages/products/list_products.tsx
+++ pages/products/list_products.tsx
@@ -3,6 +3,7 @@ import Table from "@/components/Table/Table";
 export default function ListProducts() {
   return (
     <div className="listProductWrapper">
+      <h1>Product List</h1>
       <Table />
     </div>
   );

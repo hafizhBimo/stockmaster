e1971fed07673f677bf90c84215ac0fa39dde982
hafizhBimo <hfzhbimo@gmail.com>
2023-11-27 16:01:07 +0700

    update form

--- pages/products/form.tsx
+++ pages/products/form.tsx
@@ -8,61 +8,69 @@ interface Supplier {
 }
 
 const AddProduct = () => {
-  const [formData, setFormData] = useState({
-    nama: '',
-    deskripsi: '',
-    harga: '',
-    stok: '',
-    suplier_id: '',
-    foto: null,
-  });
-
-  const [supplierList, setSupplierList] = useState<Supplier[]>([]);
-
-  useEffect(() => {
-    // Fetch supplier list from the API
-    fetch('http://localhost:3000/api/suppliers')
-      .then(response => response.json())
-      .then(data => {
-        setSupplierList(data.data);
-      })
-      .catch(error => console.error('Error fetching supplier list:', error));
-  }, []);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const form = new FormData();
-    Object.entries(formData).forEach(([key, value]) => {
-      if (value !== null) {
-        form.append(key, value);
-      }
-    });
-
-    try {
-      const response = await fetch('/api/products/create', {
-        method: 'POST',
-        body: form,
+    const [formData, setFormData] = useState({
+        nama: '',
+        deskripsi: '',
+        harga: '',
+        stok: '',
+        suplier_id: '',
+        foto: undefined as File | undefined,
       });
-
-      if (response.ok) {
-        console.log('Product added successfully');
-      } else {
-        console.error('Failed to add product');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
+    
+      const [supplierList, setSupplierList] = useState<Supplier[]>([]);
+    
+      useEffect(() => {
+        // Fetch supplier list from the API
+        fetch('http://localhost:3000/api/suppliers')
+          .then(response => response.json())
+          .then(data => {
+            setSupplierList(data.data);
+          })
+          .catch(error => console.error('Error fetching supplier list:', error));
+      }, []);
+    
+      const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+      };
+    
+      const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        setFormData((prevData) => ({
+          ...prevData,
+          foto: file,
+        }) as typeof prevData);
+      };
+    
+      const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    
+        const form = new FormData();
+        Object.entries(formData).forEach(([key, value]) => {
+          if (value !== undefined) {
+            form.append(key, value);
+          }
+        });
+    
+        try {
+          const response = await fetch('/api/products/create', {
+            method: 'POST',
+            body: form,
+          });
+    
+          if (response.ok) {
+            console.log('Product added successfully');
+          } else {
+            console.error('Failed to add product');
+          }
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+    
   return (
     <div>
       <h1>Add Product</h1>
@@ -87,6 +95,10 @@ const AddProduct = () => {
           <input type="text" name="stok" value={formData.stok} onChange={handleInputChange} />
         </label>
         <br />
+        <label>
+          Foto:
+          <input type="file" name="foto" accept="image/*" onChange={handleFileChange} />
+        </label>
         <label>
           Suplier ID:
           <select name="suplier_id" value={formData.suplier_id} onChange={handleInputChange}>
